90d37f23ee10fa0406d723bc7ecf35db547509a7
unknown <gafura891@gmail.com>
2023-07-12 15:29:52 +0700

    First time

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import FooterComponent from './components/FooterComponent'
+import NavbarComponent from './components/NavbarComponent'
+
+import Homepages from './pages/Homepages'
+import KelasPage from './pages/KelasPage'
+import TestimonialPage from './pages/TestimonialPage'
+import FaqPage from './pages/FaqPage'
+import SyaratKetenPage from './pages/SyaratKetenPage'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  return( <div> 
+    <NavbarComponent/>
+    <Routes>
+      <Route path="/" Component={Homepages}/>
+      <Route path="/kelas" Component={KelasPage}/>
+      <Route path="/testimonial" Component={TestimonialPage}/>
+      <Route path="/faq" Component={FaqPage}/>
+      <Route path="/syaratketen" Component={SyaratKetenPage}/>
+      </Routes> 
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <FooterComponent/> 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
 export default App
